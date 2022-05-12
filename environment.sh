654262a5f904b950da4e608809abb7a9abd35f2d
3omni <noticiasmundiales99@gmail.com>
2022-05-11 21:03:06 -0700

    update environ vars

--- environment.sh
+++ environment.sh
@@ -1,6 +1,6 @@
-export ROOT_PATH="$HOME/dequantizerNet"
-export DATA_PATH="$GROUP_SCRATCH/rita/data/flickr"
-export LOG_PATH="$GROUP_SCRATCH/rita/experiments"
+export ROOT_PATH="/home/users/geleta/dequantizerNet"
+export DATA_PATH="/scratch/groups/cdbustam/rita/data/flickr"
+export LOG_PATH="/scratch/groups/cdbustam/rita/experiments"
 export N_PHOTOS=100
 export CAT_FILE="categories.txt"
 export CONFIG_FILE="config.cfg"
