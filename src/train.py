043c061503d817ca797ae151e64f23aba8bddbac
3omni <noticiasmundiales99@gmail.com>
2022-05-12 14:49:52 -0700

    To adversarial training

--- src/train.py
+++ src/train.py
@@ -35,15 +35,13 @@ def set_reproductibility(seed= 2022):
 def get_params(fileName = "params.yaml"):
     with open(os.path.join(os.environ.get("ROOT_PATH"), fileName), 'r') as f:
         params = yaml.safe_load(f)
-    
-    batch_size = params["batch_size"]
     mappings = []
     with open(os.path.join(os.environ.get('ROOT_PATH'), os.environ.get('CAT_FILE'))) as f:
         for line in f:
             (key, i, img) = line.split()
             mappings.append(img)
         
-    return params, mappings, batch_size
+    return params, mappings
 
 def get_data(categories, params):
     print('Preparing data...')
@@ -56,7 +54,7 @@ def get_data(categories, params):
         max_limit=params['max_limit'],
         percentage_train=params['percentage_train']
     )
-    dataset_valid = ImageDataset(
+    dataset_test = ImageDataset(
         image_root=DATA_DIR, 
         categories=categories,
         split='test', 
@@ -72,8 +70,8 @@ def get_data(categories, params):
         num_workers=4,
         pin_memory=True
     )
-    dataloader_valid = torch.utils.data.DataLoader(
-        dataset_valid,
+    dataloader_test = torch.utils.data.DataLoader(
+        dataset_test,
         batch_size=params['batch_size'],
         shuffle=False,
         num_workers=4,
@@ -82,23 +80,39 @@ def get_data(categories, params):
     
     print('Dataset prepared.')
 
-    return dataloader_train, dataloader_valid
+    return dataloader_train, dataloader_test 
 
 def get_model_components(params):
-    model = DQNET(params)
+    ## Define adversarial models ##
+    discriminator = Discriminator(params)
+    generator = Generator(params)
+    
+    ## Send models to GPU ##
     device  = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f'Using device: {device}')
-    model = model.to(device).double()
+    discriminator = discriminator.to(device).double()
+    generator = generator.to(device).double()
     
-    criterion = nn.MSELoss() # loss functions
+    ## Print out model parameters ##
+    print('Discriminator # parameters:', sum(param.numel() for param in discriminator.parameters()))
+    print('Generator # parameters:', sum(param.numel() for param in generator.parameters()))
+    
+    ## Define adversarial loss function ##
+    criterion = nn.MSELoss() 
 
-    optimizer = optim.Adam(
+    ## Define optimizers for each network ##
+    d_optimizer = optim.Adam(
+        params=model.parameters(), 
+        lr=params['lr'],
+        weight_decay=params['weight_decay']
+    )
+    g_optimizer = optim.Adam(
         params=model.parameters(), 
         lr=params['lr'],
         weight_decay=params['weight_decay']
     )
 
-    return model, device, criterion, optimizer
+    return discriminator, generator, device, criterion, d_optimizer, g_optimizer
 
 def init_wandb(params, model):
     ## Initialize wandb
@@ -137,48 +151,92 @@ def viz2wandb(img_out, img_in, img_out_pred):
 
     return fig
 
-def gradient_step(data, optimizer, model, criterion, step, num_steps, device):
-    img_in, img_out = data[0].to(device), data[1].to(device)
-            
-    # zero the parameter gradients
-    optimizer.zero_grad()
 
-    #forward + backward + optimize
-    img_out_pred = model(img_in)
+def update_discriminator(
+    discriminator, 
+    real_img, 
+    real_label, 
+    fake_img, 
+    fake_label,
+    optimizer,
+    adv_criterion
+):
 
-    loss = criterion(img_out_pred, img_out)
+    # During discriminator model training, enable discriminator model backpropagation.
+    for d_parameters in discriminator.parameters():
+        d_parameters.requires_grad = True
 
-    wandb.log({ 'MSE train': loss })
-            
-    if step % 10 == 0:
-        print(f'{np.round(step / num_steps * 100,3)}% | TR Loss: {loss}')
-    loss.backward()
+    discriminator.zero_grad()
+    
+    # Calculate the classification score of the discriminator model for real samples:
+    real_label_pred = discriminator(real_img)
+    d_loss_real = criterion(real_label_pred, real_label)
+    
+    # Calculate the classification score of the discriminator model for fake samples:
+    fake_label_pred = discriminator(fake_img)
+    d_loss_fake = adv_criterion(fake_label_pred, fake_label)
+    
+    d_loss = d_loss_real + d_loss_fake
+    d_loss.backward(retain_graph=True)
+    
     optimizer.step()
+    
+    wandb.log({ 'd_loss train': d_loss.item() })
+    
+    # Calculate the score of the discriminator on real samples and fake samples, 
+    # the score of real samples is close to 1, and the score of fake samples is close to 0.
+    d_real_probability = torch.sigmoid_(torch.mean(real_label_pred.detach()))
+    d_fake_probability = torch.sigmoid_(torch.mean(fake_label_pred.detach()))
+    wandb.log({ 'd_real_probability train': d_real_probability.item() })
+    wandb.log({ 'd_fake_probability train': d_fake_probability.item() })
+    
+    # Before generator model training, disable discriminator model backpropagation.
+    for d_parameters in discriminator.parameters():
+        d_parameters.requires_grad = False
+    
+    return d_loss
 
-    img_in = img_in.cpu()
-    img_out = img_out.cpu()
-    img_out_pred = img_out_pred.detach().cpu()
+def update_generator(
+    generator, 
+    discriminator,
+    real_img, 
+    real_label, 
+    fake_img,
+    fake_label, 
+    optimizer, 
+    adv_criterion,
+    content_criterion,
+    params
+):
+    generator.zero_grad()
+    
+    content_loss = params['content_weight'] * content_criterion(fake_img, real_img)
+    wandb.log({ 'content_loss train': content_loss.item() })
+    adversarial_loss = params['adversarial_weight'] * adv_criterion(discriminator(fake_img), real_label)
+    wandb.log({ 'adversarial_loss train': adversarial_loss.item() })
+    g_loss = content_loss + adversarial_loss
+    g_loss.backward()
 
-    fig = viz2wandb(img_out[0,...], img_in[0,...], img_out_pred[0,...])
-    if step % 10 == 0:
-         wandb.log({f"Dequantization at step {step}": fig})
-    del img_in, img_out, img_out_pred
-    gc.collect()
-    torch.cuda.empty_cache()
-    return loss.cpu().item()
+    optimizer.step()
+    
+    wandb.log({ 'g_loss train': g_loss.item() })
+    
+    return g_loss
 
-def val_step(data, model, criterion, step, num_steps_vd, device):
+def validation(data, generator, criterion, step, device):
     
     img_in, img_out = data[0].to(device), data[1].to(device)
     
     with torch.no_grad():
-        img_out_pred = model(img_in)
-        loss = criterion(img_out_pred, img_out)
+        img_out_pred = generator(img_in).cpu()
+        #loss = criterion(img_out_pred, img_out)
     
-    wandb.log({ 'BCE valid': loss })
+    #wandb.log({ 'g_loss valid': loss })
     
     if step % 10 == 0:
-        print(f'{np.round(i / num_steps_vd * 100,3)}% | VD Loss: {loss}')
+        #print(f'VD Loss at step {step}: {loss}')
+        fig = viz2wandb(img_out[0,...], img_in[0,...], img_out_pred[0,...])
+        wandb.log({f"Dequantization at step {step}": fig})
 
     img_in = img_in.cpu()
     img_out = img_out.cpu()
@@ -187,50 +245,89 @@ def val_step(data, model, criterion, step, num_steps_vd, device):
     gc.collect()
     torch.cuda.empty_cache()
 
-    return loss.item()
+    # return loss.item()
 
 
 if __name__ == '__main__':
     set_reproductibility()
     ## Load params
-    params, categories, batch_size = get_params()
+    params, categories = get_params()
 
+    ## Create experiment folder ##
     create_folder(os.path.join(os.environ.get('LOG_PATH'), f'experiment{params["experiment"]}'))
 
-    dataloader_train, dataloader_valid = get_data(categories, params)
+    ## Load data ##
+    dataloader_train, dataloader_test = get_data(categories, params)
     
+    ## Load models, criterion and optimizer ##
+    discriminator, generator, device, criterion, d_optimizer, g_optimizer = get_model_components(params)
+
+    ## Initialize ##
+    init_wandb(params, model)
+   
     epochs = params['epochs']
     batch_size = params['batch_size']
     
-    len_dataloader_train = len(dataloader_train)
-    len_dataloader_valid = len(dataloader_valid)
-    num_steps = len_dataloader_train // batch_size + 1
-    num_steps_vd = len_dataloader_valid // batch_size + 1
+    num_steps = len(dataloader_train) // batch_size
+    num_steps_vd = len(dataloader_test) // batch_size
     
-    if len_dataloader_train == 0 or len_dataloader_valid == 0:
-        raise Exception('0 samples in the dataloader !!!')
-    
-    model, device, criterion, optimizer = get_model_components(params)
-
-    init_wandb(params, model)
-
+    ## Metrics ##
     best_loss = np.inf
-    for epoch in range(epochs): 
+    for epoch in tqdm(range(epochs), file=sys.stdout): 
        
-        model = model.train()
+        ## TRAINING LOOP #########################################################################
+        discriminator = discriminator.train()
+        generator = generator.train()
         running_loss = 0.0
-        for i, data in tqdm(enumerate(dataloader_train), file=sys.stdout):
-            running_loss += gradient_step(data, optimizer, model, criterion, i, num_steps, device)
-            tqdm.write(f'[{i+1}/{len_dataloader_train}] TR loss: {running_loss / (i+1)}') 
+        for i, data in enumerate(dataloader_train):
+            img_in, img_out = data[0].to(device), data[1].to(device)
+            img_out_pred = generator(img_in)
+            
+            # Used for the output of the discriminator binary classification, 
+            # the input sample is from the dataset (real sample) and marked as 1, 
+            # and the input sample from the generator (fake sample) is marked as 0.
+            real_label = torch.full([img_out.size(0), 1], 1.0, device=device)
+            fake_label = torch.full([img_in.size(0), 1], 0.0, device=device)
         
-        ## VALIDATION LOOP
-        model = model.eval()
+            #tqdm.write('[%d, %5d] TR loss: %.3f' % (epoch + 1, i + 1, running_loss / num_steps))
+            
+            # (1) Update D network: maximize D(x)-1-D(G(z))
+            d_loss = update_discriminator(
+                discriminator=discriminator,
+                real_img=img_out,
+                real_label=real_label,
+                fake_img=img_out_pred,
+                fake_label=fake_label,
+                optimizer=d_optimizer, 
+                adv_criterion=nn.BCEWithLogitsLoss,
+            )
+            
+            # (2) Update G network: minimize 1-D(G(z)) + Perception Loss + Image Loss + TV Loss
+            
+            img_out_pred = generator(img_in)##not sure if we must repeat this step
+            g_loss = update_generator(
+                generator=generator,
+                discriminator=discriminator,
+                real_img=img_out, 
+                fake_img=img_out_pred,
+                fake_out=fake_out,
+                optimizer=g_optimizer,
+                adv_criterion=nn.BCEWithLogitsLoss,
+                content_criterion=ContentLoss,
+                params=params
+            )
+            
+        ## VALIDATION LOOP #######################################################################
+        generator = generator.eval()
         running_loss = 0.0
-        for i, data in tqdm(enumerate(dataloader_valid), file=sys.stdout):
-            running_loss += val_step(data, model, criterion, i, num_steps_vd, device)
-            tqdm.write(f'[{i+1}/{len_dataloader_valid}] VD loss: {running_loss / (i+1)}') 
+        for i, data in enumerate(dataloader_test):
+            
+            validation(data, generator, criterion, i, device)
+            #tqdm.write('[%d, %5d] VD loss: %.3f' % (epoch + 1, i + 1, running_loss / num_steps_vd)) 
         if bool(running_loss < best_loss):
             print('Storing a new best model...')
             torch.save(model.state_dict(), os.path.join(os.environ.get('LOG_PATH'), f'experiment{params["experiment"]}/DQNET_weights_{params["experiment"]}.pt'))
+            
+        ##########################################################################################
             
     print('Finished Training!')
