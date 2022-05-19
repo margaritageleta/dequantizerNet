1ee752685845d8cddb55456011c35141b3ec4317
Pablo Martin Redondo <pmartnr@gmail.com>
2022-05-18 18:06:43 -0700

    Trying SRGAN

--- src/architecture.py
+++ src/architecture.py
@@ -172,54 +172,48 @@ class Generator2(nn.Module):
  
 class Discriminator(nn.Module):
     def __init__(self, params):
-        super().__init__()
-        self.CONV_funnel = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1),
+        super(Discriminator, self).__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
             nn.LeakyReLU(0.2),
 
             nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2),
 
-            # nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            # nn.BatchNorm2d(128),
-            # nn.LeakyReLU(0.2),
-
-            # nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1),
-            # nn.BatchNorm2d(128),
-            # nn.LeakyReLU(0.2),
-
-            # nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            # nn.BatchNorm2d(256),
-            # nn.LeakyReLU(0.2),
-
-            # nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1),
-            # nn.BatchNorm2d(256),
-            # nn.LeakyReLU(0.2),
-
-            # nn.Conv2d(256, 512, kernel_size=3, padding=1),
-            # nn.BatchNorm2d(512),
-            # nn.LeakyReLU(0.2),
-
-            # nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),
-            # nn.BatchNorm2d(512),
-            # nn.LeakyReLU(0.2),
-            nn.Conv2d(64, 1, kernel_size=1)
-        )
-        
-        self.FC_funnel = nn.Sequential(
-            nn.Linear(256, 64),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2),
-            nn.Linear(64, 1),
-            nn.Sigmoid()
+
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2),
+
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(512, 1024, kernel_size=1),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(1024, 1, kernel_size=1)
         )
 
     def forward(self, x):
-        b = x.size(0)
-        o = self.CONV_funnel(x)
-        o = o.reshape(b, -1)
-        o = self.FC_funnel(o)
-        return o
+        batch_size = x.size(0)
+        return torch.sigmoid(self.net(x).view(batch_size))
 
 class ContentLoss(nn.Module):
     
