2ffd5672c64a6e0a37b20747a215c86c59769b11
Pablo Martin Redondo <pmartnr@gmail.com>
2022-05-19 14:29:15 -0700

    Environment gdrive

--- environment.sh
+++ environment.sh
@@ -1,7 +1,7 @@
-export ROOT_PATH="/home/users/geleta/dequantizerNet"
-export DATA_PATH="/scratch/groups/cdbustam/rita/data/flickr"
-export LOG_PATH="/scratch/groups/cdbustam/rita/experiments"
-export N_PHOTOS=100
+export ROOT_PATH="/content/dequantizerNet"
+export DATA_PATH="/content/gdrive/Shareddrives/DQNData/data"
+export LOG_PATH="/content/gdrive/Shareddrives/DQNData/logs"
+export N_PHOTOS=10
 export CAT_FILE="categories.txt"
 export CONFIG_FILE="config.cfg"
 export DWD_CAT_FILE="downloaded_categories.txt"
